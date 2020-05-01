dc6264ad6c3f4d20cb8454d8b10a0b838eba6346
anshab2812 <anshab2812@gmail.com>
2020-05-01 13:34:16 +0200

    LR2

---
+++ SP_labwork2/SP_labwork2_1/SP_labwork2_1.cpp
@@ -0,0 +1,201 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>;
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+void SetDirectory(TCHAR* buffer) {
+	for (;;) {
+		cout << "Введите полный путь к директории" << endl;
+		cout << "Используйте двойные слеши как эти - C://" << endl;
+		TCHAR temp[MAX_PATH];
+		cin.get();
+		scanf("%80S", temp);
+		BOOL res = SetCurrentDirectory(temp);
+		if (!res)
+			cout << "\nНе корректное имя директории!" << endl;
+		else {
+			GetCurrentDirectory(sizeof(temp), temp);
+			cout << "Текущая директория: ";
+			wcout << temp << endl;
+			lstrcpy(buffer, L"");
+			lstrcpy(buffer, temp);
+			return;
+		}
+	}
+}
+
+void PrintDirectory(TCHAR* buffer) {
+	cout << "Файлы в текущей директории: " << endl;
+	WIN32_FIND_DATA FindFileData;
+	HANDLE hf;
+	TCHAR find[MAX_PATH];
+	lstrcpy(find, buffer);
+	lstrcat(find, L"\\*");
+	hf = FindFirstFile((LPCWSTR)find, &FindFileData);
+	if (hf != INVALID_HANDLE_VALUE)
+	{
+		do
+		{
+			SYSTEMTIME systime;
+			FILETIME localfiletime;
+			FileTimeToLocalFileTime(&FindFileData.ftCreationTime, &localfiletime);
+			FileTimeToSystemTime(&localfiletime, &systime);
+			LONGLONG size = (FindFileData.nFileSizeHigh * (MAXDWORD + 1)) + FindFileData.nFileSizeLow;
+			wcout << FindFileData.cFileName << "\t" << size << " bytes " << "\t" << systime.wDay << "." << systime.wMonth << "." << systime.wYear << " " << systime.wHour << ":" << systime.wMinute << endl;
+		} while (FindNextFile(hf, &FindFileData) != 0);
+		FindClose(hf);
+	}
+}
+
+void CopyFileInCurDir(TCHAR* buffer) {
+	BOOL k = true;
+	for (;;) {
+		cout << "Введите имя файла в текущей директории: ";
+		TCHAR find[MAX_PATH], s[MAX_PATH], newDirectory[MAX_PATH];
+		lstrcpy(find, buffer);
+		cin.get();
+		scanf("%80S", s);
+		lstrcat(find, L"\\");
+		lstrcat(find, s);
+		cout << "\nВведите директорию для копирования (E://...): ";
+		//cin.get();
+		scanf("%80S", newDirectory);
+		lstrcat(newDirectory, L"\\");
+		lstrcat(newDirectory, s);
+		k = CopyFile(find, newDirectory, false);
+		if (k) return;
+		else cout << "Не корректное имя!(s)\n";
+	}
+}
+void MyCreateDirectory(TCHAR* buffer) {
+	TCHAR newDir[MAX_PATH], way[MAX_PATH];
+	cout << "Введите новое имя директории: ";
+	cin.get();
+	scanf("%80S", newDir);
+	lstrcpy(way, buffer);
+	lstrcat(way, L"\\");
+	lstrcat(way, newDir);
+	if (CreateDirectory(way, NULL))
+		cout << "Создание произошло успешно!" << endl;
+	else
+		cout << "Ошибка при создании директории!" << endl;
+}
+
+void FullInformation(TCHAR* buffer) {
+	TCHAR file[MAX_PATH], way[MAX_PATH];
+	cout << "Введите имя файла в текущей директории: ";
+	cin.get();
+	scanf("%80S", file);
+	lstrcpy(way, buffer);
+	lstrcat(way, L"\\");
+	lstrcat(way, file);
+	HANDLE hFile1 = CreateFile(way, GENERIC_READ, NULL, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	BY_HANDLE_FILE_INFORMATION infa;
+	BOOL f = GetFileInformationByHandle(hFile1, &infa);
+	CloseHandle(hFile1);
+	if (!f) {
+		cout << "Не корректное имя файла\n";
+	}
+	else {
+		cout << "Это ";
+		if (infa.dwFileAttributes == FILE_ATTRIBUTE_HIDDEN)
+			cout << "скрыт";
+		if (infa.dwFileAttributes == FILE_ATTRIBUTE_SYSTEM)
+			cout << "системный";
+		if (infa.dwFileAttributes == FILE_ATTRIBUTE_ARCHIVE)
+			cout << "архивный";
+		if (infa.dwFileAttributes == FILE_ATTRIBUTE_COMPRESSED)
+			cout << "сжатый";
+		if (infa.dwFileAttributes == FILE_ATTRIBUTE_NORMAL)
+			cout << "нормальный";
+		cout << " файл.\n";
+		SYSTEMTIME systime;
+		FILETIME localfiletime;
+		FileTimeToLocalFileTime(&infa.ftCreationTime, &localfiletime);
+		FileTimeToSystemTime(&localfiletime, &systime);
+		cout << "Дата создания - " << systime.wDay << "." << systime.wMonth << "." << systime.wYear << " " << systime.wHour << ":" << systime.wMinute << endl;
+
+		FileTimeToLocalFileTime(&infa.ftLastWriteTime, &localfiletime);
+		FileTimeToSystemTime(&localfiletime, &systime);
+		cout << "Последние изменения - " << systime.wDay << "." << systime.wMonth << "." << systime.wYear << " " << systime.wHour << ":" << systime.wMinute << endl;
+	}
+}
+void MyDelete(TCHAR* buffer) {
+	TCHAR file[MAX_PATH], way[MAX_PATH];
+	cout << "Введите имя файла для удаления: ";
+	cin.get();
+	scanf("%80S", file);
+	lstrcpy(way, buffer);
+	lstrcat(way, L"\\");
+	lstrcat(way, file);
+	BOOL g = DeleteFile(way), k;
+	if (!g) {
+		k = RemoveDirectory(way);
+		cout << "Файл удален!";
+	}
+	if (!g && !k) {
+		DWORD lastErrorCode = GetLastError();
+		void* cstr;
+		FormatMessage(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+			NULL,
+			GetLastError(),
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPTSTR)&cstr,
+			0,
+			NULL
+		);
+		PTCHAR res((LPTSTR)cstr);
+		printf("%s", res);
+	}
+}
+
+int main() {
+
+	setlocale(LC_ALL, "rus");
+
+	while (true) {
+		TCHAR buffer[MAX_PATH];
+		GetCurrentDirectory(sizeof(buffer), buffer);
+
+		int choice;
+
+		system("cls");
+		cout << "Текущая директория: ";
+		wcout << buffer << endl;
+		cout << "\t\tМеню:" << endl;
+		cout << "0 - Выбрать директорию" << endl;
+		cout << "1 - Вывести содержимое директории" << endl;
+		cout << "2 - Копирование файла" << endl;
+		cout << "3 - Создание директории" << endl;
+		cout << "4 - Подробная информация о файле" << endl;
+		cout << "5 - Удаление файла  или пустой директории" << endl;
+		cout << "Выберите действие: ";
+
+		cin >> choice;
+
+		switch (choice) {
+		case 0:
+			SetDirectory(buffer);
+			break;
+		case 1:
+			PrintDirectory(buffer);
+			break;
+		case 2:
+			CopyFileInCurDir(buffer);
+			break;
+		case 3:
+			MyCreateDirectory(buffer);
+			break;
+		case 4:
+			FullInformation(buffer);
+			break;
+		case 5:
+			MyDelete(buffer);
+			break;
+		}
+		system("pause");
+	}
+}
---
+++ SP_labwork2/SP_labwork2_2/SP_labwork2_2.cpp
@@ -0,0 +1,219 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <windows.h>
+#include <vector>
+using namespace std;
+LPCWSTR RECB = L"records.txt";
+
+struct rec {
+	UINT number;
+	FILETIME creationTime;
+	TCHAR data[81];
+	UINT changesCounter;
+};
+struct header {
+	UINT not_Null;
+	UINT SizeFile;
+};
+
+header ReadCounts() {
+	HANDLE hFile1 = CreateFile(RECB, GENERIC_READ, NULL, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	SetFilePointer(hFile1, NULL, NULL, FILE_BEGIN);
+	header h;
+	DWORD jk;
+	ReadFile(hFile1, &h, sizeof(header), &jk, NULL);
+	if (jk == 0) {
+		h.not_Null = 0;
+		h.SizeFile = 0;
+	}
+	CloseHandle(hFile1);
+	return h;
+}
+
+vector<rec> ReadRecs() {
+	HANDLE hFile1 = CreateFile(RECB, GENERIC_READ, NULL, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	SetFilePointer(hFile1, sizeof(header), NULL, FILE_BEGIN);
+	vector<rec> datas(0);
+
+	rec i;
+	DWORD jk = 0;
+	for (;;) {
+		bool k = ReadFile(hFile1, &i, sizeof(rec), &jk, NULL);
+		if (jk == 0) break;
+		datas.push_back(i);
+	}
+	CloseHandle(hFile1);
+
+	return datas;
+}
+void PrintAllInformation() {
+	SYSTEMTIME cm;
+	vector <rec> datas = ReadRecs();
+	header count = ReadCounts();
+	cout << "Размер файла: " << count.SizeFile << " bytes; Не пустых записей: " << count.not_Null << endl;
+	for (int i = 0; i < datas.size(); i++) {
+		FileTimeToSystemTime(&datas[i].creationTime, &cm);
+		cout << "Номер = " << datas[i].number << endl;
+		cout << cm.wDay << "." << cm.wMonth << "." << cm.wYear << " " << cm.wHour << ":" << cm.wMinute << endl;
+		cout << "Данные = ";
+		wcout << datas[i].data << endl;
+		cout << "Счетчик изменений = " << datas[i].changesCounter << endl;
+		cout << "------------------------------" << endl;
+		cout << endl;
+	}
+}
+
+void Add_rec() {
+	header count = ReadCounts();
+	vector <rec> data = ReadRecs();
+	SYSTEMTIME k, s;
+	GetLocalTime(&k);
+
+	FILETIME ft;
+	rec f;
+	SystemTimeToFileTime(&k, &ft);
+	FileTimeToSystemTime(&ft, &s);
+	cout << "Введите строку: ";
+	scanf("%80S", f.data);
+
+	if (lstrcmp(f.data, L"*") == 0) {
+		f.number = data.size();
+		f.creationTime = ft;
+		lstrcpy(f.data, L"");
+		f.changesCounter = 0;
+	}
+	else {
+		f.number = data.size();
+		f.creationTime = ft;
+		f.changesCounter = 0;
+		count.not_Null++;
+	}
+
+	data.push_back(f);
+
+	count.SizeFile = sizeof(count) + sizeof(rec) * data.size();
+	HANDLE hFile1 = CreateFile(RECB, GENERIC_WRITE, NULL, NULL, TRUNCATE_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	LPDWORD temp = NULL;
+	SetFilePointer(hFile1, NULL, NULL, FILE_BEGIN);
+	WriteFile(hFile1, &count, sizeof(header), temp, NULL);
+	SetFilePointer(hFile1, NULL, NULL, FILE_END);
+	for (int i = 0; i < data.size(); i++) {
+		WriteFile(hFile1, &data[i], sizeof(rec), temp, NULL);
+	}
+	CloseHandle(hFile1);
+}
+void Change_rec() {
+	SYSTEMTIME cm;
+	header count = ReadCounts();
+	vector <rec> data = ReadRecs();
+	UINT n;
+	cout << "Введите номер записи для изменения: " << endl;
+	cin >> n;
+	cout << "\nЭто ваша запись" << endl;
+
+	FileTimeToSystemTime(&data[n].creationTime, &cm);
+
+	cout << "Номер = " << data[n].number << endl;
+	cout << cm.wDay << "." << cm.wMonth << "." << cm.wYear << " " << cm.wHour << ":" << cm.wMinute << endl;
+	cout << "Данные = ";
+	wcout << data[n].data << endl;
+	cout << "Счетчик изменений = " << data[n].changesCounter << endl;
+	cout << "------------------------------" << endl;
+	cout << endl;
+
+	cout << "\nВведите новую строку:";
+	cout << "\nДля пустой строки введите '*'): ";
+	TCHAR str[81];
+
+	lstrcpy(str, data[n].data);
+	scanf("%80S", data[n].data);
+	if (lstrcmp(data[n].data, L"*") == 0) {
+		lstrcpy(data[n].data, L"");
+	}
+	if (lstrcmp(str, L"") == 0 && lstrcmp(data[n].data, L"") != 0) {
+		count.not_Null++;
+	}
+	if (lstrcmp(str, L"") != 0 && lstrcmp(data[n].data, L"") == 0) {
+		count.not_Null--;
+	}
+	data[n].changesCounter++;
+	HANDLE hFile1 = CreateFile(RECB, GENERIC_WRITE, NULL, NULL, TRUNCATE_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	LPDWORD musor = NULL;
+
+	SetFilePointer(hFile1, NULL, NULL, FILE_BEGIN);
+	WriteFile(hFile1, &count, sizeof(header), musor, NULL);
+	SetFilePointer(hFile1, NULL, NULL, FILE_END);
+
+	for (int i = 0; i < data.size(); i++) {
+		WriteFile(hFile1, &data[i], sizeof(rec), musor, NULL);
+	}
+	CloseHandle(hFile1);
+
+}
+void delete_rec() {
+	header count = ReadCounts();
+	vector <rec> data = ReadRecs();
+	int i;
+	cout << "Введите номер записи для удаления: ";
+	cin >> i;
+
+	if (lstrcmp(data[i].data, L"") != 0)
+		count.not_Null--;
+
+	data.erase(data.begin() + i);
+	vector<rec>(data).swap(data);
+	for (int k = 0; k < data.size(); k++) {
+		data[k].number = k;
+	}
+	count.SizeFile = sizeof(count) + sizeof(rec) * data.size();
+	HANDLE hFile1 = CreateFile(RECB, GENERIC_WRITE, NULL, NULL, TRUNCATE_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	LPDWORD musor = NULL;
+
+	SetFilePointer(hFile1, NULL, NULL, FILE_BEGIN);
+	WriteFile(hFile1, &count, sizeof(header), musor, NULL);
+	SetFilePointer(hFile1, NULL, NULL, FILE_END);
+
+	for (int i = 0; i < data.size(); i++) {
+		WriteFile(hFile1, &data[i], sizeof(rec), musor, NULL);
+	}
+	CloseHandle(hFile1);
+}
+int main() {
+	setlocale(LC_ALL, "rus");
+	int key;
+	while (true) {
+		cout << "Программа для работы с записями" << endl;
+		cout << "\t\tМеню:" << endl;
+		cout << "1 - Изменить запись" << endl;
+		cout << "2 - Удалить запись" << endl;
+		cout << "3 - Добавить запись" << endl;
+		cout << "4 - Вывести все записи" << endl;
+		cout << "0 - Выход" << endl;
+		cout << "Выберите действие: ";
+		cin >> key;
+		switch (key) {
+		case 0:
+			exit(EXIT_SUCCESS);
+			break;
+		case 1:
+			Change_rec();
+			break;
+		case 2:
+			delete_rec();
+			break;
+		case 3:
+			Add_rec();
+			break;
+		case 4:
+			PrintAllInformation();
+			break;
+		default:
+			cout << "Change your choice" << endl;
+			Sleep(100);
+			system("cls");
+		}
+		system("pause");
+		system("cls");
+	}
+}
